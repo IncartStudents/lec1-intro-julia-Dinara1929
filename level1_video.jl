ce4f2ed306850247b63cf0f5eac3cc4f45a5aae6
Dinara1929 <7000152189@mail.ru>
2025-03-09 22:16:00 +0300

    Update level1_video.jl

--- level1_video.jl
+++ level1_video.jl
@@ -1,3 +1,35 @@
 # переписать ниже примеры из первого часа из видеолекции: 
 # https://youtu.be/4igzy3bGVkQ
 # по желанию можно поменять значения и попробовать другие функции
+
+#Getting Started
+println("I'm excited to learn Julia")
+
+my_answer = 42
+@show typeof(my_answer)
+my_pi = 3.14159
+@show typeof(my_pi)
+my_name = "Jane"
+@show typeof(my_name)
+
+my_answer = my_name
+@show typeof(my_answer)
+
+#comment
+
+#=
+comment
+=#
+
+sum = 3+7
+@show sum
+difference = 10-3
+@show difference
+product = 20*5
+@show product
+quotient = 100/10
+@show quotient
+power = 10^2
+@show power
+modulus = 101%2
+@show modulus
