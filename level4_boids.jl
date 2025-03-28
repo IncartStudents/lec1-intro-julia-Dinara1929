808d47667bc84a85a36d12cb99af22c3911beba9
Dinara R. Edikhanova <7000152189@mail.ru>
2025-03-28 23:35:19 +0300

    1

--- level4_boids.jl
+++ level4_boids.jl
@@ -26,7 +26,8 @@ function mid_vec(birds,n_boids,indiv_vec)
 end
 
 function walls(state::WorldState,xy::Matrix{Float64},i::Int)
-    if state.boid[1]<1
+    space = 3
+    if state.boid[1]<space
         if state.boid[2]>(state.height-state.boid[2])#xk plus; yk min
             if xy[1,i]<0
                 xy[1,i]=xy[1,i]*(-1)
@@ -43,7 +44,7 @@ function walls(state::WorldState,xy::Matrix{Float64},i::Int)
             end
         end
     end
-    if state.boid[1]>(state.width-1)
+    if state.boid[1]>(state.width-space)
         if state.boid[2]>(state.height-state.boid[2])#xk min; yk min
             if xy[1,i]>0
                 xy[1,i]=xy[1,i]*(-1)
@@ -60,7 +61,7 @@ function walls(state::WorldState,xy::Matrix{Float64},i::Int)
             end
         end
     end
-    if state.boid[2]<1
+    if state.boid[2]<space
         if state.boid[1]>(state.height-state.boid[2])#xk min; yk plus
             if xy[1,i]>0
                 xy[1,i]=xy[1,i]*(-1)
@@ -77,7 +78,7 @@ function walls(state::WorldState,xy::Matrix{Float64},i::Int)
             end
         end
     end
-    if state.boid[2]>(state.height-1)
+    if state.boid[2]>(state.height-space)
         if state.boid[2]>(state.width-state.boid[2])#xk min; yk min
             if xy[1,i]>0
                 xy[1,i]=xy[1,i]*(-1)
@@ -96,62 +97,79 @@ function walls(state::WorldState,xy::Matrix{Float64},i::Int)
     end 
 end
 
-function norm(state::WorldState,x::Float64,y::Float64)
-    x=(x/(x*x+y*y))
-    y=(y/(x*x+y*y))
-    sum_x=state.boid[1]+x
-    sum_y=state.boid[2]+y
-    d=sqrt(state.boid[1]*state.boid[1]+state.boid[2]*state.boid[2])
-    sum_d=sqrt(sum_x*sum_x+sum_y*sum_y)
-    #state.boid[1].=(sum_x/sum_d*d)
-    #state.boid[2].=(sum_y/sum_d*d)
+function norm(indiv_vec,i,xy)   
+    d=sqrt(indiv_vec[1,i]^2+indiv_vec[2,i]^2) 
+    dd=sqrt(xy[1]^2+xy[2]^2)
+
+    xy[1]=(xy[1]/dd)*0.2
+    xy[2]=(xy[2]/dd)*0.2
+
+    sum_x=indiv_vec[1,i]+xy[1]
+    sum_y=indiv_vec[2,i]+xy[2]
+    sum_d=sqrt(sum_x^2+sum_y^2)
+
+    indiv_vec[1,i]=(sum_x/sum_d*d)
+    indiv_vec[2,i]=(sum_y/sum_d*d)
 end
 
-function group(birds,n_boids,x,y)
+function group(indiv_vec,n_boids,mid_vec)
     for i in 1:n_boids
-        x1=x-birds[i].boid[1]
-        y1=y-birds[i].boid[2]
-        #norm(birds[i],x1,y2)
+        xy=zeros(2)
+        xy[1]=mid_vec[1,1]-indiv_vec[1,i]
+        xy[2]=mid_vec[1,2]-indiv_vec[2,i]
+        norm(indiv_vec,i,xy)
     end
 end
 
-function untouch(birds,i,j,n_boids)
-    d=sqrt((birds[i].boid[1]-birds[j].boid[1])^2+(birds[i].boid[2]-birds[j].boid[2])^2)
-    x=(birds[i].boid[1]-birds[j].boid[1])*0.5
-    y=(birds[i].boid[2]-birds[j].boid[2])*0.5
-    if d<1.5
-        #norm(birds[j],x,y)
-        #norm(birds[i],-x,-y)
+function untouch(birds,indiv_vec,i,j,n_boids)
+    xy=zeros(2)
+    xy[1]=(birds[j].boid[1]-birds[i].boid[1])
+    xy[2]=(birds[j].boid[2]-birds[i].boid[2])
+    
+    dlin=sqrt(xy[1]^2+xy[2]^2)
+    if dlin<3
+        di=sqrt(indiv_vec[1,i]^2+indiv_vec[2,i]^2)
+        dj=sqrt(indiv_vec[1,j]^2+indiv_vec[2,j]^2)
+
+        indiv_vec[1,i]=(indiv_vec[1,i]-xy[1]*3)
+        indiv_vec[2,i]=(indiv_vec[2,i]-xy[2]*3)
+        d=sqrt(indiv_vec[1,i]^2+indiv_vec[2,i]^2)
+        indiv_vec[1,i]=indiv_vec[1,i]/d*di
+        indiv_vec[2,i]=indiv_vec[2,i]/d*di
+
+        indiv_vec[1,j]=(indiv_vec[1,j]+xy[1]*3)
+        indiv_vec[2,j]=(indiv_vec[2,j]+xy[2]*3)
+        d=sqrt(indiv_vec[1,j]^2+indiv_vec[2,j]^2)
+        indiv_vec[1,j]=indiv_vec[1,j]/d*dj
+        indiv_vec[2,j]=indiv_vec[2,j]/d*dj
     end
 end
 
-function speed(indiv_vec,n_boids,x,y)
+function speed(indiv_vec,n_boids,xy)
+    speed=sqrt(xy[2,1]^2+xy[2,2]^2)
+    k=0.1
     for i in 1:n_boids
-        if indiv_vec[1,i]>x
-            indiv_vec[1,i]=indiv_vec[1,i]-0.1*x
-        else
-            indiv_vec[1,i]=indiv_vec[1,i]+0.1*x
-        end            
-        if indiv_vec[2,i]>y
-            indiv_vec[2,i]=indiv_vec[2,i]-0.1*y
-        else
-            indiv_vec[2,i]=indiv_vec[2,i]+0.1*y
+        d=sqrt(indiv_vec[1,i]^2+indiv_vec[2,i]^2)
+        if speed>d
+            indiv_vec[1,i]=indiv_vec[1,i]+k*xy[2,1]
+            indiv_vec[2,i]=indiv_vec[2,i]+k*xy[2,1]
+        end
+        if speed<d
+            indiv_vec[1,i]=indiv_vec[1,i]-k*xy[2,2]
+            indiv_vec[2,i]=indiv_vec[2,i]-k*xy[2,2]
         end
     end
 end
 
-function update!(state::WorldState,indiv_vec,i,mid_vec)
+function update!(state::WorldState,indiv_vec,i)
     xy=indiv_vec
     walls(state,xy,i)
-
-    norm(state,mid_vec[1,1],mid_vec[1,2])
-        
+    
     state.boid = replace(state.boid,state.boid[1]=>state.boid[1]+xy[1,i])
     state.boid = replace(state.boid,state.boid[2]=>state.boid[2]+xy[2,i])
     # TODO: реализация уравнения движения птичек
     indiv_vec[1,i]=xy[1,i]
     indiv_vec[2,i]=xy[2,i]
-    #return nothing
 end
 
 function (@main)(ARGS)
@@ -171,24 +189,25 @@ function (@main)(ARGS)
     end
 
     anim = @animate for time = 1:100
-
-        group(birds,n_boids,mid_vec(birds,n_boids,indiv_vec)[1,1],mid_vec(birds,n_boids,indiv_vec)[1,2])
+        group(indiv_vec,n_boids,mid_vec(birds,n_boids,indiv_vec))
         for i in 1:(n_boids-1)
             for j in (i+1):n_boids
-                untouch(birds,n_boids,i,j)
+                untouch(birds,indiv_vec,i,j,n_boids)
             end
         end
-        speed(indiv_vec,n_boids,mid_vec(birds,n_boids,indiv_vec)[2,1],mid_vec(birds,n_boids,indiv_vec)[2,2])
+        speed(indiv_vec,n_boids,mid_vec(birds,n_boids,indiv_vec))
             
         for i in 1:n_boids
-            update!(birds[i],indiv_vec,i,mid_vec(birds,n_boids,indiv_vec))
+            update!(birds[i],indiv_vec,i)
             x = [bird.boid[1] for bird in birds]
             y = [bird.boid[2] for bird in birds]
             scatter(x,y,xlim = (0, w), ylim = (0, h))
         end  
     end
     gif(anim, "boids.gif", fps = 10)
-    
+    #gif(anim, "boids-group.gif", fps = 10)
+    #gif(anim, "boids-untouch.gif", fps = 10)
+    #gif(anim, "boids-speed.gif", fps = 10)
 end
 
 export main
@@ -197,13 +216,3 @@ end
 
 using .Boids
 Boids.main("")
-
-#=
-function norm (since string 99)
-    state.boid[1].=(sum_x/sum_d*d)
-    state.boid[2].=(sum_y/sum_d*d)
-ERROR:
-MethodError: no method matching copyto!(::Float64, ::Base.Broadcast.Broadcasted{…})
-The function `copyto!` exists, but no method is defined for this combination of argument types.
-
-=#
